28bcdac4c53c6835b854eea09d58cb15d6a93319
smhabibjr <smhabib.abir2030@gmail.com>
2023-09-11 10:43:52 +0200

    add component

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+
+
+export default {
+  data() {
+    return {
+      message: 'Foods'
+    };
+  }
+};
+
+
+</script>
+
+<template>
+
+
+  <h1>{{ message }}</h1>
+
+  <food-item />
+  
+  <food-item />
+  
+  <food-item />
+
+</template>
+
+<style>
+
+#app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+
+  #app > div:hover {
+  cursor: pointer;
+}
+
+
+</style>
---
+++ src/components/FoodItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-on:click="clicksCounter">
+        <h1> {{ name }} </h1>
+        <p>{{ para }}</p>
+        <p>You have clickec me {{ clicks }} </p>
+    </div>
+
+
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                name: 'Apples',
+                message: 'I like apples',
+                clicks: 0
+            }
+        },
+        methods: {
+            clicksCounter(event){
+                this.clicks++;
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
